5d47dde0ff7f0377699f27fd61851fcf966a54d1
Arkan5859 <przemekkoscielski477@gmail.com>
2023-09-16 16:30:31 +0200

    I made, for now, AllGames section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
       colors: {
         'grayer': '#181818',
         'blacker': '#0E0E0E',
+        'Blue4': '#005A82',
+        'Blue2': '#0AC8B9',
       }
     },
     fontFamily: {
